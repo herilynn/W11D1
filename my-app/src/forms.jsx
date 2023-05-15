3b1fad620171d7b42f9f378be9882276660270f3
herilynn <4henrylin17@gmail.com>
2023-05-15 15:19:48 -0400

    finish part 1 of the 1st project

--- my-app/src/forms.jsx
+++ my-app/src/forms.jsx
@@ -6,6 +6,23 @@ function Form (props) {
   const [firstName, setFirstName] = useState("");
   const [email, setEmail] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
+
+  const [errorMessages, setErrorMessages] = useState([]);
+
+  const validate = () => {
+    let errors = [];
+    
+    if (firstName.length === 0) {
+      errors.push("First name can't be empty");
+    }
+    if (email.length === 0) {
+      errors.push("Email can't be empty");
+    }
+    if (phoneNumber.length !== 10) {
+      errors.push("Phone number should only be 10 digits")
+    }
+    return errors;
+  }
  
   return (
     <form>
@@ -23,9 +40,11 @@ function Form (props) {
         </div>
       </div>,
       <h1> Staff </h1>,
-      {/* <input type = "radio" id = "instructor">Instructor</input>,
-      <input type = "radio" id = "student">Student</input>, */}
+      <p class = "container"><input type = "radio" value = "instructor" checked = {props.selectOption === 'instructor'} onChange={props.handleChange} />Instructor</p>,
+      <p class = "container"><input type = "radio" value = "student" checked = {props.selectOption === 'student'} onChange={props.handleChange} />Student</p>,
       <textarea id = "bio" name = "bio" rows = "4" cols = "50"></textarea>,
+      <p class = "container"> <input type = "checkbox" /> <span class = "checkmark"></span>check for email
+      email notifications</p>
       {/* <input type = "checkbox" id = "email_notifications" name = "email_notifications">email</input> */}
     </form>
   )
